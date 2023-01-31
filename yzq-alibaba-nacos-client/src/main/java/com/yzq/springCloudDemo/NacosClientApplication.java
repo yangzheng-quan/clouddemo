19c3ff357ef2170ebb2fe31f21f0a2945d95718b
yangzhengquan <179118304@qq.com>
2023-01-31 17:23:30 +0800

    init

---
+++ yzq-alibaba-nacos-client/src/main/java/com/yzq/springCloudDemo/NacosClientApplication.java
@@ -0,0 +1,17 @@
+package com.yzq.springCloudDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ *工程启动入口
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosClientApplication {
+    public static void main(String[] args){
+        SpringApplication.run(NacosClientApplication.class,args);
+    }
+
+}
